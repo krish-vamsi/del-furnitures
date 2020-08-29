c530170e4f989f219f6ce1170242e1261d6ce104
Vamsi Krishna <t.b.vamsi.krishna@accenture.com>
2020-08-29 14:24:06 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+const $button  = document.querySelector('#sidebar-toggle');
+const $wrapper = document.querySelector('#wrapper');
+
+$button.addEventListener('click', (e) => {
+  e.preventDefault();
+  $wrapper.classList.toggle('toggled');
+});
+// search Input
+function myFunction() {
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("searchUser");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("output");
+  li = ul.getElementsByTagName("li");
+  for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("h5")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          li[i].style.display = "";
+      } else {
+          li[i].style.display = "none";
+      }
+  }
+}
+
+document.addEventListener("DOMContentLoaded",function getJson() {
+  fetch('posts.json')
+    .then(function(res){
+      return res.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      let output = '';
+      data.forEach(function(post) {
+        output += `
+        <li class="col-sm-3 col-lg-3 col-md-3 eachBox" id="button2">
+        <div>
+          <img src= '${post.photo}'class = "img-responsive" width = "100%">
+          <div class="container content">
+            <h5 class="m-0"><b>${post.title}</b></h5> 
+            <p>${post.body}</p> 
+          </div>
+        </li>
+
+</div>
+        `;
+      });
+      document.getElementById('output').innerHTML = output;
+    })
+    .catch(function(err){
+      console.log(err);
+    });
+});
+
+
+// Get local json data
+
+
+
